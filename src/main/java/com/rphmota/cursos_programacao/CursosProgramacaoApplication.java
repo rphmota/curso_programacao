8e2a312931a49b959f938486db7cadd422561ea8
Carlos Bezerra <carlos.bezerra@afinzpartners.com.br>
2024-02-26 19:26:09 -0300

    feat: initial spring commit

---
+++ src/main/java/com/rphmota/cursos_programacao/CursosProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.rphmota.cursos_programacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosProgramacaoApplication {
+
+	public static void main(String[] args) {		
+		SpringApplication.run(CursosProgramacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rphmota/cursos_programacao/CursosProgramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rphmota.cursos_programacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosProgramacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
