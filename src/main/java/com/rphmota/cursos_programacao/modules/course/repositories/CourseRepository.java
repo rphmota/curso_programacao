6e73f43431e757076080226c0f845608e8d08116
Raphael Mota <rphmota@gmail.com>
2024-03-05 22:25:15 -0300

    feat: add course repository

---
+++ src/main/java/com/rphmota/cursos_programacao/modules/course/repositories/CourseRepository.java
@@ -0,0 +1,18 @@
+package com.rphmota.cursos_programacao.modules.course.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rphmota.cursos_programacao.modules.course.entities.Course;
+import java.util.List;
+
+
+public interface CourseRepository extends JpaRepository<Course,UUID>{
+  Optional<Course>  findByName(String name);
+  
+  List<Course> findByCategory(String category);
+
+  
+}
