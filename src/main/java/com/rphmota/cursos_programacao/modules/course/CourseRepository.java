73542d85b686b2509d77a1138a90ac81eef9c95e
Raphael Mota <rphmota@gmail.com>
2024-03-05 22:25:40 -0300

    feat: move files to correct folders

--- src/main/java/com/rphmota/cursos_programacao/modules/course/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rphmota.cursos_programacao.modules.course;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,UUID> {
-  
-}
