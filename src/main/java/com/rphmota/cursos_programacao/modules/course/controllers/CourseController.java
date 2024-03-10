2b2a48d654ba3a9880cc1c45bbe569a8eee9a383
Raphael Mota <rphmota@gmail.com>
2024-03-10 19:02:56 -0300

    feat: add getmapping on controller

--- src/main/java/com/rphmota/cursos_programacao/modules/course/controllers/CourseController.java
+++ src/main/java/com/rphmota/cursos_programacao/modules/course/controllers/CourseController.java
@@ -1,20 +1,25 @@
 package com.rphmota.cursos_programacao.modules.course.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import jakarta.validation.Valid;
 
 import com.rphmota.cursos_programacao.modules.course.entities.Course;
 import com.rphmota.cursos_programacao.modules.course.useCases.CreateCourseUseCase;
 import com.rphmota.cursos_programacao.modules.course.useCases.DeleteCourseUseCase;
+import com.rphmota.cursos_programacao.modules.course.useCases.ListCourseUseCase;
 
 
 @RestController
@@ -26,6 +31,9 @@ public class CourseController {
 
   @Autowired
   private DeleteCourseUseCase deleteCourseUseCase;
+
+  @Autowired
+  private ListCourseUseCase listCourseUseCase;
 
 
   @PostMapping()
@@ -48,5 +56,8 @@ public class CourseController {
     }
   }
 
-
+  @GetMapping
+  public List<Course> findJobByFilter(@RequestParam(required = false, defaultValue = "") String filter) {
+    return this.listCourseUseCase.execute(filter);
+}
 }
