bc0bb986382d8a6806e2ef1cf0f927587fcdfe9f
Raphael Mota <rphmota@gmail.com>
2024-03-15 10:38:53 -0300

    feat: add find and update usecases for course

--- src/main/java/com/rphmota/cursos_programacao/modules/course/controllers/CourseController.java
+++ src/main/java/com/rphmota/cursos_programacao/modules/course/controllers/CourseController.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,9 @@ import jakarta.validation.Valid;
 import com.rphmota.cursos_programacao.modules.course.entities.Course;
 import com.rphmota.cursos_programacao.modules.course.useCases.CreateCourseUseCase;
 import com.rphmota.cursos_programacao.modules.course.useCases.DeleteCourseUseCase;
+import com.rphmota.cursos_programacao.modules.course.useCases.FindCourseUseCase;
 import com.rphmota.cursos_programacao.modules.course.useCases.ListCourseUseCase;
+import com.rphmota.cursos_programacao.modules.course.useCases.UpdateCourseUseCase;
 
 
 @RestController
@@ -35,6 +40,12 @@ public class CourseController {
   @Autowired
   private ListCourseUseCase listCourseUseCase;
 
+  @Autowired FindCourseUseCase findCourseUseCase;
+
+  @Autowired
+  private UpdateCourseUseCase updateCourseUseCase;
+
+
 
   @PostMapping()
   public ResponseEntity<Object> create(@Valid @RequestBody Course course){
@@ -47,7 +58,7 @@ public class CourseController {
   }
 
   @DeleteMapping("/{uuid}")
-  public ResponseEntity<Object> delete(@PathVariable UUID uuid){
+  public ResponseEntity<Object> delete(@PathVariable @NonNull UUID uuid){
     try {
       deleteCourseUseCase.execute(uuid); // Executando a deleção
       return ResponseEntity.ok().build(); // Retorna resposta 200 OK sem corpo, indicando sucesso
@@ -57,7 +68,28 @@ public class CourseController {
   }
 
   @GetMapping
-  public List<Course> findJobByFilter(@RequestParam(required = false, defaultValue = "") String filter) {
+  public List<Course> findCourseByFilter(@RequestParam(required = false, defaultValue = "") String filter) {
     return this.listCourseUseCase.execute(filter);
+  }
+  
+  @GetMapping("/{uuid}")
+  public ResponseEntity<Object> indCourseById(@RequestParam UUID uuid) {
+    try {
+      var result = this.findCourseUseCase.execute(uuid);
+      return ResponseEntity.ok().body(result);
+
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    
 }
+@PutMapping("/{uuid}")
+  public ResponseEntity<Object> update(@PathVariable @NonNull UUID uuid, @Valid @RequestBody Course course){
+    try {
+      var updatedCourse = updateCourseUseCase.execute(uuid, course);
+      return ResponseEntity.ok().body(updatedCourse);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
 }
